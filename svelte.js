595c14ac911cc0569df42d6097e5f9cb6780fe71
Alex Dilley <alex.dilley@gmail.com>
2022-01-04 21:22:05 +0000

    fix(svelte): no-multiple-empty-lines (#2)
    sveltejs/eslint-plugin-svelte3#41

--- svelte.js
+++ svelte.js
@@ -10,6 +10,7 @@ module.exports = {
       processor: 'svelte3/svelte3',
       rules: {
         'import/first': 'off',
+        'no-multiple-empty-lines': [ 'error', { max: 1, maxBOF: 2, maxEOF: 0 } ],
       },
     },
   ],
